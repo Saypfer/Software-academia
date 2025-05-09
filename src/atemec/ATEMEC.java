af8ca107d1499f03330d1e83367eec6da37633fe
Rony Tabique <Rony Tabique@HPrftb20>
2025-05-08 23:37:12 -0600

    Primera veersion

---
+++ src/atemec/ATEMEC.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atemec;
+
+/**
+ *
+ * @author Rony Tabique
+ */
+public class ATEMEC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
